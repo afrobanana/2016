e59ff05a49b03a46f7f03330d12120b7e38545b6
Nicolas R <rudasn@gmail.com>
2016-07-22 18:31:19 +0300

    xbrowser

--- utils/css.js
+++ utils/css.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+
 module.exports = function() {
   console.log('build/css');
   return gulp.src('_src/styles/*.css')
-      .pipe(cleanCSS({
-      }))
+      .pipe(cleanCSS())
+      .pipe(autoprefixer())
       .pipe(gulp.dest('styles/'));
 };
